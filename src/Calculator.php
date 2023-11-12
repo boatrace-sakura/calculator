07826e39e34842dd4555dcb3c0093803eafea9a4
shimomo <developer@shimomo.net>
2023-11-13 01:05:09 +0900

    Formatting

--- src/Calculator.php
+++ src/Calculator.php
@@ -70,9 +70,7 @@ class Calculator
     public static function getContainer(): Container
     {
         $builder = new ContainerBuilder;
-
         $builder->addDefinitions(__DIR__ . '/../config/definitions.php');
-
         return $builder->build();
     }
 }
